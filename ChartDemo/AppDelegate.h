8f0a232a365da0e40f0eb26fb581bb041ffe6c72
Vishal Gandhi <vrgandhi@Vishals-MacBook-Pro.local>
2015-01-26 11:53:04 +0530

    Initial Commit

---
+++ ChartDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChartDemo
+//
+//  Created by Vishal Gandhi on 26/01/15.
+//  Copyright (c) 2015 Vishal Gandhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChartDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChartDemo
+//
+//  Created by Vishal Gandhi on 26/01/15.
+//  Copyright (c) 2015 Vishal Gandhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
